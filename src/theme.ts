b9f3b775a3f1056b301ce05cbd87e192cba7be2f
Eitan Lavi <44952942+JlitaNemery@users.noreply.github.com>
2025-02-28 17:06:26 +0200

    fix small phones view

--- src/theme.ts
+++ src/theme.ts
@@ -4,6 +4,9 @@ import { extendTheme } from '@chakra-ui/react';
 const theme = extendTheme({
   styles: {
     global: {
+      ':where(*)': {
+        overflow: 'hidden',
+      },
       body: {
         bg: 'gray.50',
         color: 'gray.800',
